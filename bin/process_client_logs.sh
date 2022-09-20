13de0222e0a42e3e15c4cb0d6ddaa8c0d3f6fad6
BananaObserved <golds349@morris.umn.edu>
2022-09-20 15:21:58 -0500

    Fix regex error in process_client_logs

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,2 +1,2 @@
 cd "$1"
-sed -nr 's/(\w\w\w [0-9]+ [0-9][0-9]):[0-9][0-9]:[0-9][0-9] \w+ sshd\[[0-9]+\]: Failed password for (invalid user )?(\w+) from ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*$/\1 \3 \4/w failed_login_data.txt' var/log/*
+sed -nr 's/(\w\w\w)  ?([0-9]+ [0-9][0-9]):[0-9][0-9]:[0-9][0-9] \w+ sshd\[[0-9]+\]: Failed password for (invalid user )?(\w+) from ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*$/\1 \2 \4 \5/w failed_login_data.txt' var/log/*
