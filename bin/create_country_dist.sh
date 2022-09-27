bcd59e64830a901636aa94f66d57b56d481e3724
Lloyd Hilsgen <hilsg008@neoprene.morris.umn.edu>
2022-09-27 17:13:43 -0500

    AWK selects and prints the list of IP addresses

---
+++ bin/create_country_dist.sh
@@ -0,0 +1,17 @@
+## Takes in name of directory, assuming each subdiretory is the name of a computer, each containing a failed_login_data.txt
+
+scriptloc="$(pwd)"
+tmpfile=$(mktemp -d)
+filename="$1_countries.txt"
+cd "$tmpfile" || exit
+tmploc="$(pwd)"
+for file in $(ls "$scriptloc/$1")
+do
+    awk 'match($0, /([A-Z].*)([" "].*)([" "].*)([" "].*)([" "].*)/, groups) {print groups[5]}' "$scriptloc/$1/$file/failed_login_data.txt" >> "$filename"
+done
+sort "$filename" > "${filename::-4}"_sorted.txt
+awk 'match($0, /([0-9].*)/, grouping) {print grouping[0]}' "${filename::-4}"_sorted.txt >> "${filename::-4}"_data.txt
+## uniq -c "${filename::-4}"_sorted.txt > "${filename::-4}"_counted.txt
+## awk 'match($0, /([" "]+)([0-9]+)([" "]+)([0-9\.]+)/, grouping) {print "data.addRow([""\047"grouping[4]"\047"", "grouping[2]"]);"}' "${filename::-4}"_counted.txt >> "${filename::-4}"_data.txt
+cd "$scriptloc" || exit
+source bin/wrap_contents.sh "$tmploc"/"${filename::-4}"_data.txt html_components/country_dist "$1"/country_dist.html
