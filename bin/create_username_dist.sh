f42dd7a77a19f11aa748d891bc1fe2ee8ec120f1
Lloyd Hilsgen <hilsg008@neoprene.morris.umn.edu>
2022-09-27 15:47:32 -0500

    Fixed match and correctly runs wrap_contents.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -5,16 +5,13 @@ tmpfile=$(mktemp -d)
 filename="$1_usernames.txt"
 cd "$tmpfile" || exit
 tmploc="$(pwd)"
-echo "$scriptloc/$1"
-echo "$tmploc"
 for file in $(ls "$scriptloc/$1")
 do
     awk 'match($0, /([A-Z].*)([" "].*)([" "].*)([" "].*)([" "].*)/, groups) {print groups[4]}' "$scriptloc/$1/$file/failed_login_data.txt" >> "$filename"
 done	    
 sort "$filename" > "${filename::-4}"_sorted.txt
-uniq -c "${filename::-4}"_sorted.txt > "$filename"
-awk 'match($0, /([0-9].*)([" "].*)([A-Za-z0-9].*)/, grouping) {print data"."addRow("[" "\047" grouping[1] "\047" ,grouping[0]"]")}' > "${filename::-4}"_data.txt
+uniq -c "${filename::-4}"_sorted.txt > "${filename::-4}"_counted.txt
+awk 'match($0, /([0-9]+)([" "].*)([" "])([A-Za-z].*)/, grouping) {print "data.addRow([""\047"grouping[4]"\047"", "grouping[1]"]);"}' "${filename::-4}"_counted.txt >> "${filename::-4}"_data.txt
 cd "$scriptloc" || exit
-echo $(pwd)
-/bin/wrap_contents.sh "$tmploc"/"${filename::-4}"_data.txt ../username_dist username_dist.html
+source bin/wrap_contents.sh "$tmploc"/"${filename::-4}"_data.txt html_components/username_dist "$1"/username_dist.html
 
