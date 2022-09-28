c94590c1314a49a950f68ed952e46fc5a9306b29
Lloyd Hilsgen <hilsg008@neoprene.morris.umn.edu>
2022-09-28 12:33:34 -0500

    assembles report

---
+++ bin/assemble_report.sh
@@ -0,0 +1,3 @@
+tmpfile=$(mktemp)
+cat "$1/country_dist.html" "$1/hours_dist.html" "$1/username_dist.html" > "$tmpfile"
+source bin/wrap_contents.sh "$tmpfile" html_components/summary_plots $1/failed_login_summary.html
